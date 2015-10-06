1194663c0545550f60ec95581778cc73d9fb3a0f
TobiasEmilHarbo <tobiasharbo@gmail.com>
2015-10-06 13:40:25 +0200

    refaktureret pressure.c

--- pressure.c
+++ pressure.c
@@ -1,97 +1,87 @@
 #include <avr/io.h>
 #define PORT_ON(port,pin) port |= (1<<pin)
 #define PORT_OFF(port,pin) port &= ~(1<<pin)
+typedef unsigned char BYTE;
+
+unsigned int pressureA, pressureB, pressureC, pressureD; // Variable to hold ADC result
+
+int readPressure(BYTE byte)
+{
+	ADMUX = byte;//0x05; // ADC input channel set to PC5
+	ADCSRA |= (1<<ADSC); // Start conversion
+	while (ADCSRA & (1<<ADSC)); // wait for conversion to complete
+
+	return ADCW;
+}
 
 int main(void)
 {
-	//unsigned int pressure1; // Variable to hold ADC result
-	//unsigned int pressure2; // Variable to hold ADC result
+	DDRD = 0b11111111; // Set Port D as Output
+	PORTD = 0b00000000;
 
-	uint16_t pressure1, pressure2, pressure3, pressure4;
-
-	DDRD = 0b11111111;//0xff; // Set Port D as Output
-	PORTD = 0b00000000;//0x00;
-	DDRB = 0b11111111;
+	DDRB = 0b11111111; // Set Port B as Output
 	PORTB = 0b00000000;
 
 	ADCSRA = (1<<ADEN) | (1<<ADPS2) | (1<<ADPS0);
 	// ADEN: Set to turn on ADC , by default it is turned off
-	//ADPS2: ADPS2 and ADPS0 set to make division factor 32
+	// ADPS2: ADPS2 and ADPS0 set to make division factor 32
 
+	int baseThreshold = 512;
+
+	int adjustThresholdA = 0;
+	int adjustThresholdB = 0;
+	int adjustThresholdC = 0;
+	int adjustThresholdD = 0;
 
 	while (1)
 	{
-		// The four sensors uses PC1, PC2, PC3 and PC4
-		ADMUX = 0b00000001;//0x01; // ADC input channel set to PC1
+		pressureA = readPressure(0b00000000); //PC0
+		pressureB = readPressure(0b00000001); //PC1
+		pressureC = readPressure(0b00000010); //PC2
+		pressureD = readPressure(0b00000011); //PC3
 
-		ADCSRA |= (1<<ADSC); // Start conversion
-		while (ADCSRA & (1<<ADSC)); // wait for conversion to complete
-
-		pressure1 = ADCW; //Store ADC value
-
-		ADMUX = 0b00000010; //0x02; ADC input channel set to PC2
-
-		ADCSRA |= (1<<ADSC); // Start conversion
-		while (ADCSRA & (1<<ADSC)); // wait for conversion to complete
-
-		pressure2 = ADCW;
-
-		ADMUX = 0b00000011;//0x03; // ADC input channel set to PC3
-
-		ADCSRA |= (1<<ADSC); // Start conversion
-		while (ADCSRA & (1<<ADSC)); // wait for conversion to complete
-
-		pressure3 = ADCW; //Store ADC value
-
-		ADMUX = 0b00000100; //0x04; ADC input channel set to PC4
-
-		ADCSRA |= (1<<ADSC); // Start conversion
-		while (ADCSRA & (1<<ADSC)); // wait for conversion to complete
-
-		pressure4 = ADCW;
-
-
-		if (pressure1 < 800)
+		if (pressureA < (baseThreshold + adjustThresholdA))
 		{
-			PORT_OFF(PORTD,7); // Toggle LEDs
-			PORT_ON (PORTD,6);
+			PORT_OFF(PORTB,0);
+			PORT_ON(PORTD,7);
 		}
 		else
 		{
-			PORT_ON(PORTD,7); // Toggle LEDs
-			PORT_OFF (PORTD,6);
+			PORT_ON(PORTB,0);
+			PORT_OFF(PORTD,7);
 		}
 
-		if(pressure2 < 800)
+		if (pressureB < (baseThreshold + adjustThresholdB))
+		{
+			PORT_ON(PORTD,5);
+			PORT_OFF(PORTD,6);
+		}
+		else
+		{
+			PORT_OFF(PORTD,5); // Toggle LEDs
+			PORT_ON(PORTD,6);
+		}
+
+		if (pressureC < (baseThreshold + adjustThresholdC))
+		{
+			PORT_OFF(PORTB,7);
+			PORT_ON(PORTB,6);
+		}
+		else
+		{
+			PORT_ON(PORTB,7); // Toggle LEDs
+			PORT_OFF(PORTB,6);
+		}
+
+		if (pressureD < (baseThreshold + adjustThresholdD))
 		{
 			PORT_OFF(PORTB,1);
 			PORT_ON(PORTB,2);
 		}
 		else
 		{
-			PORT_ON(PORTB,1);
+			PORT_ON(PORTB,1); // Toggle LEDs
 			PORT_OFF(PORTB,2);
-		}
-		//TODO: Decide what ports to use for the LED
-		if (pressure3 < 800)
-		{
-			PORT_OFF(PORTD,0); // Toggle LEDs
-			PORT_ON(PORTD,1);
-		}
-		else
-		{
-			PORT_ON(PORTD,0); // Toggle LEDs
-			PORT_OFF (PORTD,1);
-		}
-		if(pressure4 < 800)
-		{
-			PORT_OFF(PORTD,2);
-			PORT_ON(PORTD,3);
-		}
-		else
-		{
-			PORT_ON(PORTD,2);
-			PORT_OFF(PORTD,3);
 		}
 	}
 }
