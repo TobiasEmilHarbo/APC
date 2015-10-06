7caba4ce6f79f56442014b52e0d891eb38c0bfae
Ultreia- <201304196@post.au.dk>
2015-10-06 10:36:23 +0200

    A lil bit o code refactoring

--- xbee.c
+++ xbee.c
@@ -2,18 +2,17 @@
 
 #include <util/delay.h>
 #include <avr/io.h>
-#include <avr/interrupt.h>                                    // set the CPU clock
 
 //Method declarations
 void receiveData(void); 
-void transmitData(void);
+unsigned char uart_receiveByte (void);
 
-void uart_transmit (unsigned char);
-unsigned char uart_receive (void);
+void transmitData(unsigned char);
+void uart_transmitByte (unsigned char);
 
 // define baudrate for serial communication
-#define BAUD 9600                                   // define baud
-#define BAUDRATE ((F_CPU)/(BAUD*16UL)-1)            // set baud rate value for UBRR
+#define BAUD 9600                               // define baud
+#define BAUDRATE ((F_CPU)/(BAUD*16UL)-1)        // set baud rate value for UBRR
   
 unsigned char data;
 
@@ -26,15 +25,13 @@ void uart_init (void)
     UCSRC|= (1<<URSEL)|(1<<UCSZ0)|(1<<UCSZ1);   // 8bit data format
 }
 
-
-void uart_transmit (unsigned char data)
+void uart_transmitByte (unsigned char data)
 {
-    while (!( UCSRA & (1<<UDRE)));              // wait while register is free
+    while (!(UCSRA & (1<<UDRE)));              // wait while register is free
     UDR = data;                                 // load data in the register
 }
 
-
-unsigned char uart_receive (void)
+unsigned char uart_receiveByte (void)
 {
         while(!(UCSRA) & (1<<RXC));             // wait while data is being received
         return UDR;                             // return 8-bit data
@@ -49,17 +46,17 @@ int main (void)
 	
 	while(1){
 		receiveData();
-		transmitData();
+		//transmitData();
+		_delay_ms(500);
 	}
 }
 
 void receiveData (void)
 {
-
-	data = uart_receive();
+	data = uart_receiveByte();
 
 	if(data=='1'){
-		PORTC=0b00100000;						//turn on LED if recieved character is 'a'
+		PORTC=0b00100000;						//turn on LED if recieved character is '1'
 	}
 	else 
 	{	
@@ -67,10 +64,7 @@ void receiveData (void)
 	}
 }
 
-
-
-void transmitData (void)
+void transmitData (unsigned char data)
 {
-	uart_transmit('1');
-	_delay_ms(1000);
+	uart_transmitByte(data);
 }
