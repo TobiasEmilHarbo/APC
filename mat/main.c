edc15dd8bf2f0a81dc7f16897fc55d991c565dce
Ultreia- <201304196@post.au.dk>
2015-10-16 15:27:37 +0200

    Changed FCPU to fit 8L and included RX/TX functionality

--- mat/main.c
+++ mat/main.c
@@ -1,10 +1,42 @@
+#define F_CPU 4000000UL 
+
+// define baudrate for serial communication
+#define BAUD 9600                               
+#define BAUDRATE ((F_CPU)/(BAUD*16UL)-1)
+
 #include <avr/io.h>
+ 
 #define PORT_ON(port,pin) port |= (1<<pin)
 #define PORT_OFF(port,pin) port &= ~(1<<pin)
 typedef unsigned char BYTE;
 
 unsigned int pressureA, pressureB, pressureC, pressureD; // Variable to hold ADC result
 int lvlOfDanger, dangerLvlOne, dangerLvlTwo, dangerLvlThree;
+unsigned char transmitData, receivedData;
+
+void uart_transmitByte (unsigned char);
+void uart_receiveByte (void);
+
+// function to initialize UART
+void uart_init (void)
+{
+    UBRRH = (BAUDRATE>>8);                      // shift the register right by 8 bits
+    UBRRL = BAUDRATE;                           // set baud rate
+    UCSRB|= (1<<TXEN)|(1<<RXEN);      			// enable receiver and transmitter
+    UCSRC|= (1<<URSEL)|(1<<UCSZ0)|(1<<UCSZ1);   // 8bit data format
+}
+
+void uart_transmitByte (unsigned char data)
+{
+    while (!(UCSRA & (1<<UDRE)));              // wait while register is free
+    UDR = transmitData;                                 // load data in the register
+}
+
+void uart_receiveByte (void)
+{
+	while(!(UCSRA) & (1<<RXC));             		// wait while data is being received
+	receivedData = UDR;                             // return 8-bit data
+}
 
 int readPressure(BYTE byte)
 {
@@ -51,19 +83,20 @@ void readDangerLvl()
 	}*/
 	if(dangerLvlThree > 0 || dangerLvlTwo > 1)
 	{
-		PORT_ON(PORTD,1);
+		uart_transmitByte('2');
 	}
 	else if(dangerLvlTwo > 0)
 	{
-		PORT_ON(PORTD,2);
+		uart_transmitByte('1');
 	}
+	/*
 	else if(dangerLvlOne > 0)
 	{
 		PORT_ON(PORTD,3);
-	}
+	}*/
 	else
 	{
-		PORT_ON(PORTD,4);
+		uart_transmitByte('0');
 	}
 
 	/*switch(lvlOfDanger)
@@ -90,6 +123,8 @@ int main(void)
 
 	DDRB = 0b11111111; // Set Port B as Output
 	PORTB = 0b00000000;
+
+	uart_init();
 
 	ADCSRA = (1<<ADEN) | (1<<ADPS2) | (1<<ADPS0);
 	// ADEN: Set to turn on ADC , by default it is turned off
