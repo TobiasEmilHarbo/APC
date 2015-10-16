3c10cfb4ca0988c5466bffcb87773f93a0ddb560
Ultreia- <201304196@post.au.dk>
2015-10-16 19:02:07 +0200

    stuff

--- xbee.c
+++ xbee.c
@@ -1,26 +1,25 @@
-#define F_CPU 8000000UL 
+#define F_CPU 4000000UL 
 
 // define baudrate for serial communication
 #define BAUD 9600                               
 #define BAUDRATE ((F_CPU)/(BAUD*16UL)-1)
 
+#define PORT_ON(port,pin) port |= (1<<pin)
+#define PORT_OFF(port,pin) port &= ~(1<<pin)
+
 #include <util/delay.h>
 #include <avr/io.h>
-
+#include <avr/interrupt.h>
 
 //Method declarations
-void receiveData(void); 
-
 void uart_transmitByte (unsigned char);
-void readSignalOnPin(void);
+void uart_receiveByte (void);
 
 typedef unsigned char BYTE;
-
 int readSignalStrength(BYTE byte);
-unsigned int signalStrength; // Variable to hold ADC result
-unsigned char uart_receiveByte (void);
-unsigned char data, i;
-unsigned char message[10];
+
+unsigned char data, i, receivedData;
+int prevPulse = 0;	
 
 // function to initialize UART
 void uart_init (void)
@@ -31,66 +30,86 @@ void uart_init (void)
     UCSRC|= (1<<URSEL)|(1<<UCSZ0)|(1<<UCSZ1);   // 8bit data format
 }
 
+
 void uart_transmitByte (unsigned char data)
 {
     while (!(UCSRA & (1<<UDRE)));              // wait while register is free
     UDR = data;                                 // load data in the register
 }
 
-unsigned char uart_receiveByte (void)
+void uart_receiveByte (void)
 {
-	while(!(UCSRA) & (1<<RXC));             // wait while data is being received
-	return UDR;                             // return 8-bit data
+	while(!(UCSRA) & (1<<RXC));             		// wait while data is being received
+	receivedData = UDR;                             // return 8-bit data
+}
+/*
+void StartTimer1(void)
+{
+	TCNT1=0;
+	//Start timer with prescaller 1024
+	TCCR1B = (1<<CS12)|(0<<CS11)|(1<<CS10);
 }
 
-int main (void)
+void StopTimer1(void)
 {
-	DDRB= 0b11111111;									
-	PORTB=0b00000000;									
+	TCCR1B = (0<<CS12)|(0<<CS11)|(0<<CS10);
 	
+	if(TCNT1 > 10000){
+		uart_transmitByte('c');	
+	}
+}*/
+
+int main( void)
+{
 	uart_init();
-	 
-	ADCSRA = (1<<ADEN) | (1<<ADPS2) | (1<<ADPS0);       // ADEN: Set to turn on ADC , by default it is turned off
-        												// ADPS2: ADPS2 and ADPS0 set to make division factor 32
-	
-	while(1){
-		//readSignalOnPin();
 
-		receiveData();
-		
-		signalStrength = readSignalStrength(0b00000011);
+	DDRC  = 0b11111111; // Set Port C as Output
+	PORTC = 0b00000000;
+    
+    while(1)
+    {
+    	uart_receiveByte();	
 
-		sprintf(message, "%d", signalStrength); //Convert signalStrength to array of chars
-		
-		for(i=0;message[i];i++)
-		{		
-			uart_transmitByte(message[i]);
+
+    	PORT_OFF(PORTC,5);
+    	PORT_OFF(PORTC, 4);
+    	PORT_OFF(PORTC, 3);
+    	
+    	if(receivedData == '0')
+    	{
+    		PORT_ON(PORTC,5);
+    		PORT_OFF(PORTC, 4);
+    		PORT_OFF(PORTC, 3);
+    	}
+    	else if(receivedData == '1')
+    	{
+    		PORT_ON(PORTC,5);
+    		PORT_ON(PORTC, 4);
+    		PORT_OFF(PORTC, 3);
+    	}
+    	
+    	else if(receivedData == '2')
+    	{
+    		PORT_ON(PORTC,5);
+    		PORT_ON(PORTC, 4);
+    		PORT_ON(PORTC, 3);
+    	}
+
+		/*if(bit_is_set(PINB, 0)){
+			
+			if (prevPulse == 0)
+			{
+				prevPulse = 1;
+				StartTimer1();
+			}
 		}
-		
-		_delay_ms(1000);
+		if(bit_is_clear(PINB, 0))
+			if (prevPulse == 1)
+			{
+				prevPulse = 0;
+				StopTimer1();
+			}*/
 	}
 }
 
-void receiveData (void)
-{
-
-	data = uart_receiveByte();
-
-	if(data=='1'){
-		PORTB=0b00000010;						//turn on LED if received character is '1'
-	}
-	else 
-	{	
-		PORTB=0b00000000;	
-	}
-}
-
-int readSignalStrength(BYTE byte)
-{
-        ADMUX = byte;// ADC input channel set to pin specified by 'byte'
-        ADCSRA |= (1<<ADSC); // Start conversion
-        while (ADCSRA & (1<<ADSC)); // wait for conversion to complete
- 
-        return ADCW;
-}
 
