c03aa19c971de71a55eea6bb82ca865a4b0a11c5
Ultreia- <201304196@post.au.dk>
2015-10-20 11:27:54 +0200

    Now with comments

--- rssi.c
+++ rssi.c
@@ -10,12 +10,16 @@
 #include <util/delay.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <XBee.h>
 
 //Method declarations
 void uart_transmitByte (unsigned char);
 void uart_receiveByte (void);
+void uart_init (void);
 void matTest (void);
 void vibrate (char);
+void StartTimer1(void);
+void StopTimer1(void);
 
 typedef unsigned char BYTE;
 int readSignalStrength(BYTE byte);
@@ -35,44 +39,31 @@ void uart_init (void)
 
 void uart_transmitByte (unsigned char data)
 {
-    while (!(UCSRA & (1<<UDRE)));              // wait while register is free
+    while (!(UCSRA & (1<<UDRE)));               // wait while register is free
     UDR = data;                                 // load data in the register
 }
 
 void uart_receiveByte (void)
 {
-	while(!(UCSRA) & (1<<RXC));             		// wait while data is being received
-	receivedData = UDR;                             // return 8-bit data
+	while(!(UCSRA) & (1<<RXC));             	// wait while data is being received
+	receivedData = UDR;                         // return 8-bit data
 }
 
 void StartTimer1(void)
 {
-	TCNT1=0;
-	//Start timer with prescaller 1024
-	TCCR1B |= (1<<CS12)|(0<<CS11)|(1<<CS10);
-	//uart_transmitByte('s');
+	TCNT1=0;                                    //Set timer counter to 0	
+	TCCR1B |= (1<<CS12)|(0<<CS11)|(1<<CS10);    //Start 16-bit timer with a prescale of 1024
 }
 
 void StopTimer1(void)
 {
-	TCCR1B |= (0<<CS12)|(0<<CS11)|(0<<CS10);
-	
-	//sprintf(message, "%d", TCNT1); //Convert signalStrength to array of chars
-
-	/*for(i=0;message[i];i++)
-		{
-			uart_transmitByte(message[i]);
-		}
-	*/
+	TCCR1B |= (0<<CS12)|(0<<CS11)|(0<<CS10);    //Stop the timer
 	
 	if(TCNT1 > 10000){
-		uart_transmitByte('c');		// If close, only transmit c
+		uart_transmitByte('c');		            // If close, only transmit c
 	}
 	else{
-		//uart_transmitByte('f');
-		//_delay_ms(100);
-
-		matTest(); 					// If far away, call matTest
+		matTest(); 					            // If far away, call matTest
 	}
 }
 
@@ -80,18 +71,18 @@ int main( void)
 {
 	uart_init();
 
-	DDRC  = 0b11111111; // Set Port C as Output
+	DDRC  = 0b11111111;                         // Set Port C as Output
 	PORTC = 0b00000000;
 
-	DDRB  = 0b11111110; // Set Port C as Output
+	DDRB  = 0b11111110;                         // Set Port C as Output
 	PORTB = 0b00000001;
     
     while(1)
     {
 
-    	uart_receiveByte();	
+    	uart_receiveByte();	                    //Receive whatever is transmitted from mattress
 
-		if(bit_is_set(PINB, 0)){
+		if(bit_is_set(PINB, 0)){                //If receiving something (RSSI pin is high), call timer-methods
 			
 			if (prevPulse == 0)
 			{
@@ -113,28 +104,17 @@ int main( void)
 
 void matTest(void)
 {
-    	// LEDs
-    	PORT_OFF(PORTC,5);
-    	PORT_OFF(PORTC, 4);
-    	PORT_OFF(PORTC, 3);
     	
-    	if(receivedData == '1')		// The value of the received data determines vibration mode
-    	{
-    		PORT_ON(PORTC,5);
-    		PORT_ON(PORTC, 4);
-    		PORT_OFF(PORTC, 3);
+	if(receivedData == '1')		                // The value of the received data determines vibration mode
+	{
+		vibrate('1');
+	}
+	
+	else if(receivedData == '2')		        // The value of the received data determines vibration mode
+	{
 
-    		vibrate('1');
-    	}
-    	
-    	else if(receivedData == '2')		// The value of the received data determines vibration mode
-    	{
-    		PORT_ON(PORTC,5);
-    		PORT_ON(PORTC, 4);
-    		PORT_ON(PORTC, 3);
-
-    		vibrate('2');
-    	}
+		vibrate('2');
+	}
 }
 
 void vibrate (char i)
@@ -142,14 +122,7 @@ void vibrate (char i)
 
 	switch(i)
     {
-    /*case '0' :
-            //vibration
-            PORT_ON(PORTB,1);
-	
-            break;
-    */
-    case '1' :
-            //6 second vibration
+    case '1' :                                  //6 second vibration, slow pulse
             PORT_ON(PORTB,1);
             _delay_ms(1000);
             PORT_OFF(PORTB,1);
@@ -166,8 +139,7 @@ void vibrate (char i)
             _delay_ms(1000);
 
             break;
-    case '2' :
-            //6 second vibration
+    case '2' :                                  //6 second vibration, fast pulse
             PORT_ON(PORTB,1);
             _delay_ms(250);
             PORT_OFF(PORTB,1);
@@ -198,7 +170,6 @@ void vibrate (char i)
             PORT_OFF(PORTB,1);
             _delay_ms(250);
 
-                        //6 second vibration
             PORT_ON(PORTB,1);
             _delay_ms(250);
             PORT_OFF(PORTB,1);
